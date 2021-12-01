43f1eab7573b86b496bcdbd8c22c46495d7ea12f
Agudroid <agudonio@gmail.com>
2021-12-01 19:03:27 +0100

    Activities created

---
+++ app/src/main/java/com/example/work_out_/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.work_out_;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/work_out_/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.work_out_;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
