54e81c7d67d497c89528e3f061f426f7ce4b294f
Juliette Bonnard <juliette99bonnard@hotmail.fr>
2021-12-05 17:49:09 +0100

    Brightness seekbar functional

--- app/src/main/java/com/example/work_out_/ConfigActivity.java
+++ app/src/main/java/com/example/work_out_/ConfigActivity.java
@@ -1,14 +1,78 @@
 package com.example.work_out_;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.provider.Settings.System;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ConfigActivity extends AppCompatActivity {
 
+    private SeekBar seekbar;
+    private int brightness;
+    private ContentResolver cResolver;
+    private Window window;
+    WindowManager.LayoutParams layoutParams;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_config);
+
+        seekbar = findViewById(R.id.seekBar);
+        seekbar.setOnSeekBarChangeListener(new SeekbarListener());
+
+        window = getWindow();
+        cResolver =  getContentResolver();
+
+        layoutParams = window.getAttributes();
+        layoutParams.screenBrightness = brightness / (float)255;
+        window.setAttributes(layoutParams);
+
+
+        try {
+            brightness = System.getInt(cResolver, System.SCREEN_BRIGHTNESS);
+
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        seekbar.setProgress(brightness);
+
+        if(!Settings.System.canWrite(getApplicationContext())){
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            startActivity(intent);
+        }
+
     }
+
+    private class SeekbarListener implements SeekBar.OnSeekBarChangeListener {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            System.putInt(cResolver, System.SCREEN_BRIGHTNESS, progress);
+            layoutParams.screenBrightness = progress / (float)100;
+            window.setAttributes(layoutParams);
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/work_out_/MainActivity.java
+++ app/src/main/java/com/example/work_out_/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.work_out_;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -16,12 +19,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_config);
+
+        Intent secondActivityIntent = new Intent(this, ConfigActivity.class);
+        startActivity(secondActivityIntent);
     }
 }
