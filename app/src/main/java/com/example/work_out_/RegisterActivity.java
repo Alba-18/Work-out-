5345f22cf82d24a602b718992e7634737dca494b
hellboy1511 <sergiodiaz5332@gmail.com>
2021-12-08 16:42:50 +0100

    RegisterActivity & User class

--- app/src/main/java/com/example/work_out_/RegisterActivity.java
+++ app/src/main/java/com/example/work_out_/RegisterActivity.java
@@ -4,14 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.work_out_.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,13 +21,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
+import com.example.work_out_.model.User;
+
 import java.util.Objects;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
 
     private FirebaseAuth mAuth;
-    private EditText nameView, emailView, ageView, passView;
-
+    private EditText nameView, emailView, ageView, passView, weightView, heightView;
+    private Spinner levelOfExerciseSpinner, cardioSpinner, exerciseImpactSpinner, objetiveSpinner;
+    private String registerLevelOfExercise, registerCardio, registerExerciseImpact, registerObjetive;
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
@@ -40,6 +45,77 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         Button registerButtonView = (Button) findViewById(R.id.Register_FinishButton);
         registerButtonView.setOnClickListener(this);
+
+        //Creation of Spinners
+        //Creation of Profile Level of Exercise Spinner
+        this.levelOfExerciseSpinner = (Spinner) findViewById(R.id.Register_LevelOfExercise);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> levelOfExerciseAdapter = ArrayAdapter.createFromResource(this,
+                R.array.levelOfExerciseArray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        levelOfExerciseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        this.levelOfExerciseSpinner.setAdapter(levelOfExerciseAdapter);
+        this.levelOfExerciseSpinner.setOnItemSelectedListener(this);
+
+        //Creation of Cardio Spinner
+        this.cardioSpinner = (Spinner) findViewById(R.id.Register_CardioExercise);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> cardioAdapter = ArrayAdapter.createFromResource(this,
+                R.array.cardioArray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        cardioAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        this.cardioSpinner.setAdapter(cardioAdapter);
+        this.cardioSpinner.setOnItemSelectedListener(this);
+
+        //Creation of Exercise Impact Spinner
+        this.exerciseImpactSpinner = (Spinner) findViewById(R.id.Register_ExerciseImpact);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> exerciseImpactAdapter = ArrayAdapter.createFromResource(this,
+                R.array.levelOfExerciseArray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        cardioAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        this.exerciseImpactSpinner.setAdapter(exerciseImpactAdapter);
+        this.exerciseImpactSpinner.setOnItemSelectedListener(this);
+
+        //Creation of Objetive Spinner
+        this.objetiveSpinner = (Spinner) findViewById(R.id.registerObjetive);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> objetiveAdapter = ArrayAdapter.createFromResource(this,
+                R.array.objetiveArray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        cardioAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        this.objetiveSpinner.setAdapter(objetiveAdapter);
+        this.objetiveSpinner.setOnItemSelectedListener(this);
+    }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        switch (parent.getId()) {
+            case R.id.Register_LevelOfExercise:
+                this.registerLevelOfExercise = (String) parent.getItemAtPosition(pos);
+                break;
+
+            case R.id.Register_CardioExercise:
+                this.registerCardio = (String) parent.getItemAtPosition(pos);
+                break;
+
+            case R.id.Register_ExerciseImpact:
+                this.registerExerciseImpact = (String) parent.getItemAtPosition(pos);
+                break;
+
+            case R.id.registerObjetive:
+                this.registerObjetive = (String) parent.getItemAtPosition(pos);
+                break;
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 
     public void onClick(View v){
@@ -54,6 +130,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         String email = emailView.getText().toString().trim();
         String age = ageView.getText().toString().trim();
         String pass = passView.getText().toString().trim();
+        String height = heightView.getText().toString().trim();
+        String weight = weightView.getText().toString().trim();
 
         StringEmptyAndError(name,nameView);
         StringEmptyAndError(email,emailView);
@@ -76,16 +154,16 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if(task.isSuccessful()){
-                            User user = new User(name,email,age,null);
+                            User user = new User(name,email,age, weight, height, registerLevelOfExercise, registerExerciseImpact, registerCardio, registerObjetive);
 
                             FirebaseDatabase.getInstance().getReference("users")
                                     .child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
-                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
 
                                     if(task.isSuccessful()){
-                                        Toast.makeText(RegisterActivity.this, "User has beenn registered succesfully",Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(RegisterActivity.this, "User has been registered successfully",Toast.LENGTH_SHORT).show();
                                         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                                         if(user.isEmailVerified()){
--- app/src/main/java/com/example/work_out_/model/User.java
+++ app/src/main/java/com/example/work_out_/model/User.java
@@ -2,25 +2,21 @@ package com.example.work_out_.model;
 
 public class User {
 
-    private String name,email,age;
-    private String level;
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
+    private String name,email,age, weight, height, levelOfExercise, cardio, exerciseImpact, objetive;
 
     public User() {
     }
 
-    public User(String name, String email, String age, String level) {
+    public User(String name, String email, String age, String weight, String height, String levelOfExercise,String exerciseImpact, String cardio,  String objetive) {
         this.name = name;
         this.email = email;
         this.age = age;
-        this.level = level;
+        this.weight = weight;
+        this.height = height;
+        this.levelOfExercise = levelOfExercise;
+        this.cardio = cardio;
+        this.exerciseImpact = exerciseImpact;
+        this.objetive = objetive;
     }
 
     public String getName() {
@@ -45,5 +41,53 @@ public class User {
 
     public void setAge(String age) {
         this.age = age;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getLevelOfExercise() {
+        return levelOfExercise;
+    }
+
+    public void setLevelOfExercise(String levelOfExercise) {
+        this.levelOfExercise = levelOfExercise;
+    }
+
+    public String getCardio() {
+        return cardio;
+    }
+
+    public void setCardio(String cardio) {
+        this.cardio = cardio;
+    }
+
+    public String getExerciseImpact() {
+        return exerciseImpact;
+    }
+
+    public void setExerciseImpact(String exerciseImpact) {
+        this.exerciseImpact = exerciseImpact;
+    }
+
+    public String getObjetive() {
+        return objetive;
+    }
+
+    public void setObjetive(String objetive) {
+        this.objetive = objetive;
     }
 }
