4a1d2d08456c15d968b6f59154ce65e136abf7c2
Alba-18 <albasevi8@gmail.com>
2021-12-13 19:29:26 +0100

    Alba_Changes

--- app/src/main/java/com/example/work_out_/activities/PushupsStartActivity.java
+++ app/src/main/java/com/example/work_out_/activities/PushupsStartActivity.java
@@ -2,8 +2,11 @@ package com.example.work_out_.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +34,12 @@ public class PushupsStartActivity extends AppCompatActivity implements View.OnCl
     private FirebaseUser user;
     private DatabaseReference reference;
     private String userID;
+
+    Button btn_open_popUp;
+    Button btn_close;
+    LayoutInflater layoutInflater;
+    View popupView;
+    PopupWindow popupWindow;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,6 +70,25 @@ public class PushupsStartActivity extends AppCompatActivity implements View.OnCl
 
             }
         });
+
+        btn_open_popUp = (Button)findViewById(R.id.HelpPushUp);
+        btn_open_popUp.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                layoutInflater =(LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                popupView = layoutInflater.inflate(R.layout.activity_activity_popup, null);
+                popupWindow = new PopupWindow(popupView, RadioGroup.LayoutParams.WRAP_CONTENT,
+                        RadioGroup.LayoutParams.WRAP_CONTENT);
+                btn_close = (Button)popupView.findViewById(R.id.id_close);
+                btn_close.setOnClickListener(new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }});
+
+                popupWindow.showAsDropDown(btn_open_popUp);
+
+            }});
     }
 
     private void setUpText(String difficult,int[] setUps){
--- app/src/main/java/com/example/work_out_/activities/WalkingStartActivity.java
+++ app/src/main/java/com/example/work_out_/activities/WalkingStartActivity.java
@@ -1,6 +1,11 @@
 package com.example.work_out_.activities;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -9,11 +14,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.work_out_.R;
 
 public class WalkingStartActivity extends AppCompatActivity {
+
+    Button btn_open_popUp;
+    Button btn_close;
+    LayoutInflater layoutInflater;
+    View popupView;
+    PopupWindow popupWindow;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walking_start);
         setUpText();
+
+        btn_open_popUp = (Button)findViewById(R.id.HelpWalking);
+        btn_open_popUp.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                layoutInflater =(LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                popupView = layoutInflater.inflate(R.layout.activity_activity_popup, null);
+                popupWindow = new PopupWindow(popupView, RadioGroup.LayoutParams.WRAP_CONTENT,
+                        RadioGroup.LayoutParams.WRAP_CONTENT);
+                btn_close = (Button)popupView.findViewById(R.id.id_close);
+                btn_close.setOnClickListener(new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }});
+
+                popupWindow.showAsDropDown(btn_open_popUp);
+
+            }});
     }
 
     private void setUpText(){
